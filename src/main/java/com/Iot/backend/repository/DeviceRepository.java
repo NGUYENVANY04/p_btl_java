b52c0b879f3bdf64b610d8097ac81f366ed01284
NGUYENVANY04 <ychuot1122004@gmail.com>
2026-04-17 09:13:44 +0700

    donev2 tach responsitive

--- src/main/java/com/Iot/backend/repository/DeviceRepository.java
+++ src/main/java/com/Iot/backend/repository/DeviceRepository.java
@@ -1,63 +1,178 @@
-// import java.util.List;
-// import java.util.Map;
+package com.Iot.backend.repository;
 
-// import org.springframework.stereotype.Repository;
-// import org.springframework.web.client.RestTemplate;
-// import org.springframework.core.ParameterizedTypeReference;
-// import org.springframework.http.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-// @Repository
-// public class DeviceRepository {
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 
-// private final RestTemplate restTemplate;
+@Repository
+public class DeviceRepository {
 
-// private final String URL =
-// "https://znfxhbrkabxenuzrcogd.supabase.co/rest/v1";
-// private final String API_KEY = "sb_secret_NXFJy_AuCYhqJmmJadDKNA_I7N91qFu";
+    private final RestTemplate restTemplate;
 
-// public DeviceRepository(RestTemplate restTemplate) {
-// this.restTemplate = restTemplate;
-// }
+    private final String URL = "https://znfxhbrkabxenuzrcogd.supabase.co/rest/v1";
+    private final String API_KEY = "sb_secret_NXFJy_AuCYhqJmmJadDKNA_I7N91qFu";
 
-// private HttpHeaders createHeaders() {
-// HttpHeaders headers = new HttpHeaders();
-// headers.set("apikey", API_KEY);
-// headers.set("Authorization", "Bearer " + API_KEY);
-// headers.setContentType(MediaType.APPLICATION_JSON);
-// headers.set("Prefer", "return=representation");
-// return headers;
-// }
+    public DeviceRepository(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
-// public List<Map<String, Object>> insertDevice(Map<String, Object> device) {
+    private HttpHeaders createHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("apikey", API_KEY);
+        headers.set("Authorization", "Bearer " + API_KEY);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Prefer", "return=representation");
 
-// String url = URL + "/devices";
+        return headers;
+    }
 
-// HttpEntity<Map<String, Object>> entity = new HttpEntity<>(device,
-// createHeaders());
+    public Map<String, Object> createDevice(Map<String, Object> device) {
+        try {
 
-// ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
-// url,
-// HttpMethod.POST,
-// entity,
-// new ParameterizedTypeReference<List<Map<String, Object>>>() {
-// });
+            String url = URL + "/devices";
+            System.out.println(url);
+            HttpEntity<Map<String, Object>> entity = new HttpEntity<>(device, createHeaders());
 
-// return response.getBody();
-// }
+            ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.POST,
+                    entity,
+                    new ParameterizedTypeReference<List<Map<String, Object>>>() {
+                    });
 
-// public List<Map<String, Object>> getDevices() {
+            return response.getBody() != null && !response.getBody().isEmpty()
+                    ? response.getBody().get(0)
+                    : new HashMap<>();
 
-// String url = URL + "/devices";
+        } catch (Exception e) {
+            System.out.println("CREATE ERROR: " + e.getMessage());
+            return new HashMap<>();
+        }
+    }
 
-// HttpEntity<Void> entity = new HttpEntity<>(createHeaders());
+    public Map<String, Object> createLimitDevice(Long id, Map<String, Object> device) {
+        try {
 
-// ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
-// url,
-// HttpMethod.GET,
-// entity,
-// new ParameterizedTypeReference<List<Map<String, Object>>>() {
-// });
+            String url = URL + "/device_limits?id=eq." + id;
+            System.out.println(url);
+            HttpEntity<Map<String, Object>> entity = new HttpEntity<>(device, createHeaders());
+            ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.PUT,
+                    entity,
+                    new ParameterizedTypeReference<List<Map<String, Object>>>() {
+                    });
 
-// return response.getBody();
-// }
-// }
+            return response.getBody() != null && !response.getBody().isEmpty()
+                    ? response.getBody().get(0)
+                    : new HashMap<>();
+
+        } catch (Exception e) {
+            System.out.println("CREATE ERROR: " + e.getMessage());
+            return new HashMap<>();
+        }
+    }
+
+    public List<Map<String, Object>> getAllLimitDevices() {
+        try {
+            String url = URL + "/device_limits?select=*";
+
+            HttpEntity<String> entity = new HttpEntity<>(createHeaders());
+
+            ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    new ParameterizedTypeReference<List<Map<String, Object>>>() {
+                    });
+
+            return response.getBody() != null
+                    ? response.getBody()
+                    : new ArrayList<>();
+
+        } catch (Exception e) {
+            System.out.println("GET ERROR: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    public List<Map<String, Object>> getAllDevices() {
+        try {
+            String url = URL + "/devices?select=*";
+
+            HttpEntity<String> entity = new HttpEntity<>(createHeaders());
+
+            ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    new ParameterizedTypeReference<List<Map<String, Object>>>() {
+                    });
+
+            return response.getBody() != null
+                    ? response.getBody()
+                    : new ArrayList<>();
+
+        } catch (Exception e) {
+            System.out.println("GET ERROR: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    public Map<String, Object> updateDevice(Long id, Map<String, Object> device) {
+        try {
+
+            String url = URL + "/devices?id=eq." + id;
+            System.out.println(" SEND: " + url);
+            Map<String, Object> body = new HashMap<>();
+            body.put("name", device.get("name"));
+            body.put("location", device.get("location"));
+            body.put("status", true);
+
+            System.out.println("BODY SEND: " + body);
+
+            HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, createHeaders());
+
+            ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.PATCH,
+                    entity,
+                    new ParameterizedTypeReference<List<Map<String, Object>>>() {
+                    });
+
+            return response.getBody() != null && !response.getBody().isEmpty()
+                    ? response.getBody().get(0)
+                    : new HashMap<>();
+
+        } catch (Exception e) {
+            System.out.println("UPDATE ERROR: " + e.getMessage());
+            return new HashMap<>();
+        }
+    }
+
+    public String deleteDevice(Long id) {
+        try {
+            String url = URL + "/devices?id=eq." + id;
+
+            HttpEntity<String> entity = new HttpEntity<>(createHeaders());
+
+            restTemplate.exchange(
+                    url,
+                    HttpMethod.DELETE,
+                    entity,
+                    String.class);
+
+            return "Deleted successfully";
+
+        } catch (Exception e) {
+            System.out.println("DELETE ERROR: " + e.getMessage());
+            return "Delete failed";
+        }
+    }
+}
--- src/main/java/com/Iot/backend/service/DeviceService.java
+++ src/main/java/com/Iot/backend/service/DeviceService.java
@@ -1,174 +1,42 @@
 package com.Iot.backend.service;
 
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import com.Iot.backend.repository.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import java.util.*;
 
 @Service
 public class DeviceService {
 
-        private final RestTemplate restTemplate;
+        private final DeviceRepository repository;
 
-        public DeviceService(RestTemplate restTemplate) {
-                this.restTemplate = restTemplate;
+        public DeviceService(DeviceRepository repository) {
+                this.repository = repository;
         }
 
-        private final String URL = "https://znfxhbrkabxenuzrcogd.supabase.co/rest/v1";
-        private final String API_KEY = "sb_secret_NXFJy_AuCYhqJmmJadDKNA_I7N91qFu";
-
-        private HttpHeaders createHeaders() {
-
-                HttpHeaders headers = new HttpHeaders();
-                headers.set("apikey", API_KEY);
-                headers.set("Authorization", "Bearer " + API_KEY);
-                headers.setContentType(MediaType.APPLICATION_JSON);
-                headers.set("Prefer", "return=representation");
-                return headers;
-        }
-
+        // ===================== DEVICE =====================
         public Map<String, Object> createDevice(Map<String, Object> device) {
-                try {
-
-                        String url = URL + "/devices";
-                        System.out.println(url);
-                        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(device, createHeaders());
-
-                        ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
-                                        url,
-                                        HttpMethod.POST,
-                                        entity,
-                                        new ParameterizedTypeReference<List<Map<String, Object>>>() {
-                                        });
-
-                        return response.getBody() != null && !response.getBody().isEmpty()
-                                        ? response.getBody().get(0)
-                                        : new HashMap<>();
-
-                } catch (Exception e) {
-                        System.out.println("CREATE ERROR: " + e.getMessage());
-                        return new HashMap<>();
-                }
-        }
-
-        public Map<String, Object> createLimitDevice(Long id, Map<String, Object> device) {
-                try {
-
-                        String url = URL + "/device_limits?id=eq." + id;
-                        System.out.println(url);
-                        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(device, createHeaders());
-                        ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
-                                        url,
-                                        HttpMethod.PUT,
-                                        entity,
-                                        new ParameterizedTypeReference<List<Map<String, Object>>>() {
-                                        });
-
-                        return response.getBody() != null && !response.getBody().isEmpty()
-                                        ? response.getBody().get(0)
-                                        : new HashMap<>();
-
-                } catch (Exception e) {
-                        System.out.println("CREATE ERROR: " + e.getMessage());
-                        return new HashMap<>();
-                }
-        }
-
-        public List<Map<String, Object>> getAllLimitDevices() {
-                try {
-                        String url = URL + "/device_limits?select=*";
-
-                        HttpEntity<String> entity = new HttpEntity<>(createHeaders());
-
-                        ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
-                                        url,
-                                        HttpMethod.GET,
-                                        entity,
-                                        new ParameterizedTypeReference<List<Map<String, Object>>>() {
-                                        });
-
-                        return response.getBody() != null
-                                        ? response.getBody()
-                                        : new ArrayList<>();
-
-                } catch (Exception e) {
-                        System.out.println("GET ERROR: " + e.getMessage());
-                        return new ArrayList<>();
-                }
+                return repository.createDevice(device);
         }
 
         public List<Map<String, Object>> getAllDevices() {
-                try {
-                        String url = URL + "/devices?select=*";
-
-                        HttpEntity<String> entity = new HttpEntity<>(createHeaders());
-
-                        ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
-                                        url,
-                                        HttpMethod.GET,
-                                        entity,
-                                        new ParameterizedTypeReference<List<Map<String, Object>>>() {
-                                        });
-
-                        return response.getBody() != null
-                                        ? response.getBody()
-                                        : new ArrayList<>();
-
-                } catch (Exception e) {
-                        System.out.println("GET ERROR: " + e.getMessage());
-                        return new ArrayList<>();
-                }
+                return repository.getAllDevices();
         }
 
         public Map<String, Object> updateDevice(Long id, Map<String, Object> device) {
-                try {
-
-                        String url = URL + "/devices?id=eq." + id;
-                        System.out.println(" SEND: " + url);
-                        Map<String, Object> body = new HashMap<>();
-                        body.put("name", device.get("name"));
-                        body.put("location", device.get("location"));
-                        body.put("status", true);
-
-                        System.out.println("BODY SEND: " + body);
-
-                        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, createHeaders());
-
-                        ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
-                                        url,
-                                        HttpMethod.PATCH,
-                                        entity,
-                                        new ParameterizedTypeReference<List<Map<String, Object>>>() {
-                                        });
-
-                        return response.getBody() != null && !response.getBody().isEmpty()
-                                        ? response.getBody().get(0)
-                                        : new HashMap<>();
-
-                } catch (Exception e) {
-                        System.out.println("UPDATE ERROR: " + e.getMessage());
-                        return new HashMap<>();
-                }
+                return repository.updateDevice(id, device);
         }
 
         public String deleteDevice(Long id) {
-                try {
-                        String url = URL + "/devices?id=eq." + id;
+                return repository.deleteDevice(id);
+        }
 
-                        HttpEntity<String> entity = new HttpEntity<>(createHeaders());
+        // ===================== LIMIT DEVICE =====================
 
-                        restTemplate.exchange(
-                                        url,
-                                        HttpMethod.DELETE,
-                                        entity,
-                                        String.class);
+        public Map<String, Object> createLimitDevice(Long id, Map<String, Object> device) {
+                return repository.createLimitDevice(id, device);
+        }
 
-                        return "Deleted successfully";
-
-                } catch (Exception e) {
-                        System.out.println("DELETE ERROR: " + e.getMessage());
-                        return "Delete failed";
-                }
+        public List<Map<String, Object>> getAllLimitDevices() {
+                return repository.getAllLimitDevices();
         }
 }
